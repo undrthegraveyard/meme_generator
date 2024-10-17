f9eaf3f66d306bd566233ebd10c970e7c9993766
Shivam <shivamagarwalmarch@gmail.com>
2024-10-17 16:27:20 +1100

    Updating styles

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
 import '../App.less'
+import React, { useState } from 'react'
 
 export default function MainContent() {
-  return(
+  const [meme, setMeme] = useState({
+    topText: '',
+    bottomText: '',
+    randomImage: "https://i.imgflip.com/30b1gx.jpg"
+  })
+
+  return (
     <main className="main-content">
       <div className='form'>
         <label className='form--label'>
           Top Text
-        <input type='text' id='topText' placeholder='Top text' className='form--input' />
+          <input 
+            type='text' 
+            placeholder='Top text' 
+            className='form--input'
+            value={meme.topText}
+            onChange={(e) => setMeme(prevMeme => ({...prevMeme, topText: e.target.value}))}
+          />
         </label>
         <label className='form--label'>
           Bottom Text
-        <input type='text' id='bottomText' placeholder='Bottom text' className='form--input' />
+          <input 
+            type='text' 
+            placeholder='Bottom text!' 
+            className='form--input'
+            value={meme.bottomText}
+            onChange={(e) => setMeme(prevMeme => ({...prevMeme, bottomText: e.target.value}))}
+          />
         </label>
       </div>
-      <button className='form--button'>Get a new meme image  🖼</button>
+      <button className='form--button'>Get a new meme image 🖼</button>
+      <div className='meme'>
+        <img src={meme.randomImage} alt='A meme image' className='meme-image' />
+        <h2 className='meme-text top'>{meme.topText}</h2>
+        <h2 className='meme-text bottom'>{meme.bottomText}</h2>
+      </div>
     </main>
   )
 }
